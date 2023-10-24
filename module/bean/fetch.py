05482b71878d8c02a7c9e677d3bc60281d36416a
FuQiang <fuq15394@gmail.com>
2023-10-24 15:39:01 +0800

    origin: mei

--- module/bean/fetch.py
+++ module/bean/fetch.py
@@ -4,8 +4,14 @@
 # FileName:
 
 import random
+import threading
 
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
 agent_list = [
@@ -24,8 +30,8 @@ def get_user_agent():
 
 class Fetch:
 
-    def __init__(self, headless=True): ...
-        # self.browser = Browser(headless)
+    def __init__(self, headless=True):
+        self.browser = Browser(headless)
 
     @classmethod
     def request(cls, url, method='GET', headers=None, again=1, **kwargs):
@@ -59,3 +65,92 @@ class Fetch:
                 num += 1
 
         return result
+
+    def fetch_response_with_selenium(self, url, *args, **kwargs):
+        """
+        selenium模拟浏览器请求
+        :param url:
+        :return:
+        """
+        return self.browser.fetch_response(url, *args, **kwargs)
+
+
+class Browser:
+
+    _lock = threading.RLock()
+
+    def __new__(cls, *args, **kwargs):
+        # 构造单例
+        if hasattr(cls, 'instance'):
+            return cls.instance
+
+        # 线程锁
+        with cls._lock:
+            if not hasattr(cls, 'instance'):
+                cls.instance = super(Browser, cls).__new__(cls)
+            return cls.instance
+
+    def __init__(self, headless=True):
+        """
+
+        :param headless: selenium——无界面模式
+        """
+        self.headless = headless
+        self._driver = None
+
+    def __load_driver(self):
+        if self._driver is None:
+            chrome_options = Options()
+            # linux环境所需
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('start-maximized')
+            chrome_options.add_argument("--disable-extensions")
+            chrome_options.add_argument('--disable-browser-side-navigation')
+            chrome_options.add_argument('enable-automation')
+            chrome_options.add_argument('--disable-infobars')
+            chrome_options.add_argument('enable-features=NetworkServiceInProcess')
+            if self.headless:
+                # 无界面模式
+                chrome_options.add_argument('--headless')
+
+            # if config.WebDriverPath:
+            #     self._driver = webdriver.Chrome(executable_path=config.WebDriverPath, options=chrome_options)
+            # else:
+            self._driver = webdriver.Chrome(options=chrome_options)
+
+    def get_driver(self):
+        if self._driver is None:
+            self.__load_driver()
+        return self._driver
+
+    def fetch_response(self, url, until=None, until_not=None):
+        driver = self.get_driver()
+        driver.get(url)
+
+        until_not_conf, until_conf = [], []
+        until = until if until else {}
+        until_not = until_not if until_not else {}
+        if until.get('class_tag'):
+            until_conf.append((By.CLASS_NAME, until['class_tag']))
+        if until.get('id_tag'):
+            until_conf.append((By.ID, until['id_tag']))
+        if until_not.get('class_tag'):
+            until_not_conf.append((By.CLASS_NAME, until_not['class_tag']))
+        if until_not.get('id_tag'):
+            until_not_conf.append((By.ID, until_not['id_tag']))
+        if not until_conf:
+            WebDriverWait(driver, 10)
+        else:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(*until_conf)
+            )
+        if until_not_conf:
+            WebDriverWait(driver, 10).until_not(
+                EC.presence_of_element_located(*until_not_conf)
+            )
+        return driver.page_source
+
+    def __del__(self):
+        if self._driver:
+            self._driver.quit()
